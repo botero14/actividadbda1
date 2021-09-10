32daa4198c8f880d16b7c99af45262a41df19e2e
jaferck <90366562+jaferck@users.noreply.github.com>
2021-09-10 07:20:02 -0500

    Add files via upload

---
+++ tarea1.py
@@ -0,0 +1,15 @@
+def impar(num):
+    if num % 2 == 0:
+        print("El numero es par")
+    else:
+        print("El numero es impar")
+def main():
+    num = int(input("Ingrese numero: "))
+    impar(num)
+if __name__ == "__main__":
+    main()
+
+
+
+
+
