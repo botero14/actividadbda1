7f37d723d671d57fddf19829612f3d914b1305fb
Bryan Otero <botero@uniguajira.edu.co>
2021-09-10 06:53:33 -0500

    Los 4 puntos funcionan correctamente

--- actividad1.py
+++ actividad1.py
@@ -1,4 +1,9 @@
+import os
+import math
 
+def clear():
+    os.system("cls")
+ 
 def impar(num):
     if num % 2 == 0:
         print("El numero es par")
@@ -10,21 +15,42 @@ def perfecto(num):
     for i in range(1,num-1):
         if num % i == 0:
             divisores.append(i)
-        print(i)
-    print(divisores)
     suma = 0
     for i in divisores:
-        suma = suma + i
-        print(i)
-    print(suma)
+        suma = suma + i  
     if suma == num:
         print("El numero es perfecto")
     else: 
         print("El numero no es perfecto")
+
+def primo(num):
+    if num < 1:
+        return False
+
+    elif num == 2:
+        return True
+
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True   
+def exp(num):
+    suma=0
+    for n in range(0,50):
+        suma+=math.pow(num,n)/math.factorial(n)
+    print("Exponencial: ",suma)
+
 def main():
     num = int(input("Ingrese numero: "))
+    clear()
     impar(num)
     perfecto(num)
+    if primo(num): 
+        print("Es primo")
+    else: 
+        print("No es primo")
+    exp(num)
 
 
 if __name__ == "__main__":
